6c157de3b49194a21c0449d068fdfe6ccfeedc13
James Stevens <admin@skyrootstud.io>
2023-02-17 04:27:28 -0700

    potential fix for iphone 10

--- ios/Classes/SwiftSoundStreamPlugin.swift
+++ ios/Classes/SwiftSoundStreamPlugin.swift
@@ -198,7 +198,7 @@ public class SwiftSoundStreamPlugin: NSObject, FlutterPlugin {
     private func resetEngineForRecord() {
         mAudioEngine.inputNode.removeTap(onBus: mRecordBus)
         let input = mAudioEngine.inputNode
-        let inputFormat = input.outputFormat(forBus: mRecordBus)
+        let inputFormat = input.inputFormat(forBus: mRecordBus)
         let converter = AVAudioConverter(from: inputFormat, to: mRecordFormat!)!
         let ratio: Float = Float(inputFormat.sampleRate)/Float(mRecordFormat.sampleRate)
         
